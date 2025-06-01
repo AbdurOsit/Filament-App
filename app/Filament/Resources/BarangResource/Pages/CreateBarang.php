087baa3c665d5fe9932d92e3cc10c0d4882ae970
AbdurOsit <abdurrosyidmojokertopuri@gmail.com>
2025-06-01 19:17:23 +0700

    Notifications

--- app/Filament/Resources/BarangResource/Pages/CreateBarang.php
+++ app/Filament/Resources/BarangResource/Pages/CreateBarang.php
@@ -5,8 +5,25 @@ namespace App\Filament\Resources\BarangResource\Pages;
 use App\Filament\Resources\BarangResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Filament\Notifications\Notification;
 
 class CreateBarang extends CreateRecord
 {
     protected static string $resource = BarangResource::class;
+    // protected function getCreatedNotificationTitle(): ?string
+    // {
+    //     return 'Barang berhasil ditambahkan';
+    // }
+
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Berhasil Ditambahkan')
+            ->icon('heroicon-o-shield-check')
+            ->iconColor('success')
+            ->color('success')
+            ->duration(5000)
+            ->body('Barang berhasil ditambahkan');
+    }
 }
